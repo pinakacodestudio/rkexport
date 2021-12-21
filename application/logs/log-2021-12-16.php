b53ffba040b413671913ed26258ce5fb5dc74c9c
Awesome Infosys <mubin@truetec.in>
2021-12-21 15:49:54 +0530

    Gitignore added

--- application/logs/log-2021-12-16.php
+++ application/logs/log-2021-12-16.php
@@ -72,3 +72,13 @@ AND (`p`.`producttype` = '0' OR '0' = '0')
 GROUP BY `p`.`id`
 ORDER BY `p`.`id` DESC
  LIMIT 50
+ERROR - 2021-12-16 18:54:30 --> Query error: Table 'rkexport.frontendmenu' doesn't exist - Invalid query: SELECT `fm`.`id`, `fm`.`name`, IF(fm.url='', `mwc`.`slug`, fm.url) as url, `fm`.`menuicon`, `fm`.`coverimage`, IFNULL((SELECT 1 FROM frontendsubmenu as fsm WHERE fsm.frontendmenuid=fm.id AND fsm.status=1 LIMIT 1), 0) as submenuavailable
+FROM `frontendmenu` as `fm`
+LEFT JOIN `managewebsitecontent` as `mwc` ON `mwc`.`frontendmenuid`=`fm`.`id` AND `mwc`.`frontendsubmenuid`=0
+WHERE `fm`.`status` = 1 AND `fm`.`channelid` = '0' AND `fm`.`memberid` = '0'
+ORDER BY `fm`.`priority` ASC
+ERROR - 2021-12-16 18:54:30 --> Query error: Table 'rkexport.frontendmenu' doesn't exist - Invalid query: SELECT `fm`.`id`, `fm`.`name`, IF(fm.url='', `mwc`.`slug`, fm.url) as url, `fm`.`menuicon`, `fm`.`coverimage`, IFNULL((SELECT 1 FROM frontendsubmenu as fsm WHERE fsm.frontendmenuid=fm.id AND fsm.status=1 LIMIT 1), 0) as submenuavailable
+FROM `frontendmenu` as `fm`
+LEFT JOIN `managewebsitecontent` as `mwc` ON `mwc`.`frontendmenuid`=`fm`.`id` AND `mwc`.`frontendsubmenuid`=0
+WHERE `fm`.`status` = 1 AND `fm`.`channelid` = '0' AND `fm`.`memberid` = '0'
+ORDER BY `fm`.`priority` ASC
